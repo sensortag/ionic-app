8f8ca5526b73cdb3a465cb5fd604ec53b377e0ea
braunjon <braunjon@students.zhaw.ch>
2017-10-29 00:02:03 +0200

    Adding temperature-sensor test

--- src/tests/pages/model/temperature-sensor.spec.ts
+++ src/tests/pages/model/temperature-sensor.spec.ts
@@ -14,31 +14,30 @@ describe('temperature-sensor', () => {
     expect(config[0]).toBe(0x01);
   });
 
-  it('should convert the temperature', () => {
-    let tempData = new Uint32Array(1);
-    tempData[0] = 2450; //24.50 ℃
-
-    let pressureData = new Uint32Array(1);
-    pressureData[0] = 98000; //980 hPa mBar
-
-    let data = new Uint8Array(6);
-    // [0:7]
-    data[0] = tempData[0] & 0xFF;
-    // [8:15]
-    data[1] = (tempData[0] & 0xFF00) >>> 8;
-    // [16:23]
-    data[2] = (tempData[0] & 0xFF0000) >>> 16;
-
-    // [0:7]
-    data[3] = pressureData[0] & 0xFF;
-    // [8:15]
-    data[4] = (pressureData[0] & 0xFF00) >>> 8;
-    // [16:23]
-    data[5] = (pressureData[0] & 0xFF0000) >>> 16;
+  it('temperature should be zero', () => {
+    let data = new Uint8Array(4);
 
     temperatureSensor.convertData(data);
-    expect(temperatureSensor.getTemperatureAsString()).toBe('24.50 ℃');
-    expect(temperatureSensor.getPressureAsString()).toBe('980 mBar');
+
+    expect(temperatureSensor.getAmbientTemperatureAsString()).toBe('0.00 ℃');
+    expect(temperatureSensor.getAmbientTemperatureAsString()).toBe('0.00 ℃');
+  });
+
+  it('should convert the temperature', () => {
+    let ambientTemp = new Uint16Array(1);
+    ambientTemp[0] = (26.50 / 0.03125) << 2;
+
+    let irTemperature = new Uint16Array(1);
+    irTemperature[0] = (19.13 / 0.03125) << 2;
+
+    let data = new Uint16Array(2);
+    data[0] = irTemperature[0];
+    data[1] = ambientTemp[0];
+
+    temperatureSensor.convertData(data);
+
+    expect(temperatureSensor.getAmbientTemperatureAsString()).toBe('26.50 ℃');
+    expect(temperatureSensor.getInfraRedTemperatureAsString()).toBe('19.13 ℃');
   });
 
 });

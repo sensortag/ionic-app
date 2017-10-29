a468f211e8dbfb99111167fb7617cbb34bd55bbc
braunjon <braunjon@students.zhaw.ch>
2017-10-29 13:07:07 +0100

    Added optical-sensor

---
+++ src/pages/model/optical-sensor.ts
@@ -0,0 +1,56 @@
+import {Sensor} from "./sensor";
+
+/**
+ * The optical sensor in the SensorTag.
+ * http://processors.wiki.ti.com/index.php/CC2650_SensorTag_User's_Guide#Optical_Sensor
+ */
+export class OpticalSensor extends Sensor {
+
+  private illuminance: number = 0;
+
+  constructor() {
+    super(
+      'f000aa70-0451-4000-b000-000000000000',
+      'f000aa71-0451-4000-b000-000000000000',
+      'f000aa72-0451-4000-b000-000000000000',
+      'f000aa73-0451-4000-b000-000000000000'
+    );
+  }
+
+  getIlluminanceAsString(): string {
+    return this.illuminance.toFixed(2) + ' lx'
+  }
+
+  /**
+   *
+   * @returns {ArrayBuffer} 1Byte
+   */
+  public getConfigurationValue(): ArrayBuffer {
+    return new Uint8Array([0x01]).buffer; //0x01 = enable barometer
+  }
+
+  /**
+   * The parameter should have the following structure:
+   * LightLSB:LightMSB
+   *
+   * @param data 2Bytes
+   */
+  public convertData(data: any) {
+    let rawData = new Uint16Array(data);
+
+    this.illuminance = OpticalSensor.sensorOpt3001Convert(rawData[0]);
+
+  }
+
+  private static sensorOpt3001Convert(rawData: number) {
+    let m = rawData & 0x0FFF;
+    let e = (rawData & 0xF000) >> 12;
+
+    // e on 4 bits stored in a 16 bit unsigned => it can store 2 << (e - 1) with e < 16
+    e = (e == 0) ? 1 : 2 << (e - 1);
+
+    return m * (0.01 * e);
+  }
+
+
+}
---
+++ src/tests/pages/model/optical-sensor.spec.ts
@@ -0,0 +1,35 @@
+import {OpticalSensor} from "../../../pages/model/optical-sensor";
+
+describe('optical-sensor', () => {
+  let opticalSensor: OpticalSensor;
+
+  beforeEach(() => {
+    opticalSensor = new OpticalSensor();
+  });
+
+  it('should have the configuration value 0x01', () => {
+    expect(opticalSensor.getConfigurationValue().byteLength).toBe(1);
+
+    let config = new Int8Array(opticalSensor.getConfigurationValue());
+    expect(config[0]).toBe(0x01);
+  });
+
+  it('lux should be zero', () => {
+    let data = new Uint16Array(1);
+    data[0] = 0;
+
+    opticalSensor.convertData(data);
+
+    expect(opticalSensor.getIlluminanceAsString()).toBe('0.00 lx');
+  });
+
+  it('should convert the optical value to lux', () => {
+    let data = new Uint16Array(1);
+    data[0] = 9942;
+
+    opticalSensor.convertData(data);
+
+    expect(opticalSensor.getIlluminanceAsString()).toBe('70.00 lx');
+  });
+
+});

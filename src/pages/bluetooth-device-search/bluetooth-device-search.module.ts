3dee218d2bb7deb693158be3a58dabb1e2cbe5e1
braunjon <braunjon@students.zhaw.ch>
2017-11-11 14:14:06 +0100

    Add ios support

--- src/pages/bluetooth-device-search/bluetooth-device-search.module.ts
+++ src/pages/bluetooth-device-search/bluetooth-device-search.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {IonicPageModule} from 'ionic-angular';
 import {BluetoothDeviceSearchPage} from './bluetooth-device-search';
 import {BLE} from "@ionic-native/ble";
+import {Diagnostic} from '@ionic-native/diagnostic';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import {BLE} from "@ionic-native/ble";
   ],
   providers: [
     BLE,
+    Diagnostic,
   ]
 })
 export class BluetoothDeviceSearchPageModule {

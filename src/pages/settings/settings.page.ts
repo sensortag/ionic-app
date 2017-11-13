8458cdbaf597d6821b9dd0dbe54df27a7fae3e67
braunjon <braunjon@students.zhaw.ch>
2017-11-13 23:33:19 +0100

    Update variable name

--- src/pages/settings/settings.page.ts
+++ src/pages/settings/settings.page.ts
@@ -15,7 +15,7 @@ export class SettingsPage {
   private config: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
-              private configuration: SettingsService,
+              private settingsService: SettingsService,
               private toastCtrl: ToastController) {
 
     this.config = this.formBuilder.group({
@@ -23,11 +23,11 @@ export class SettingsPage {
       bluetoothFilter:[1, Validators.required]
     });
 
-    this.configuration.getSetting(SettingKeys.IP_ADDRESS).then(value => {
+    this.settingsService.getSetting(SettingKeys.IP_ADDRESS).then(value => {
       this.config.patchValue({ipAddress: <boolean> value});
     });
 
-    this.configuration.getSetting(SettingKeys.IS_BLUETOOTH_FILTER_ON).then(value => {
+    this.settingsService.getSetting(SettingKeys.IS_BLUETOOTH_FILTER_ON).then(value => {
       this.config.patchValue({bluetoothFilter: value});
     });
 
@@ -37,8 +37,8 @@ export class SettingsPage {
    * Save settings.
    */
   saveEvent() {
-    this.configuration.setSetting(SettingKeys.IP_ADDRESS, this.config.value.ipAddress);
-    this.configuration.setSetting(SettingKeys.IS_BLUETOOTH_FILTER_ON, this.config.value.bluetoothFilter);
+    this.settingsService.setSetting(SettingKeys.IP_ADDRESS, this.config.value.ipAddress);
+    this.settingsService.setSetting(SettingKeys.IS_BLUETOOTH_FILTER_ON, this.config.value.bluetoothFilter);
 
     let toast = this.toastCtrl.create({
       message: 'settings saved',

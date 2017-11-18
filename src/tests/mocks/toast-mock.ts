10ff19195842b960667b77ef0aff64290f78d86d
braunjon <braunjon@students.zhaw.ch>
2017-11-18 17:06:35 +0100

    Adding ToatControllerMock (lost commit)

---
+++ src/tests/mocks/toast-mock.ts
@@ -0,0 +1,8 @@
+export class ToastMock {
+  public static instance(): any {
+    let instance = jasmine.createSpyObj('Toast', ['present', 'dismissAll', 'setContent', 'setSpinner']);
+    instance.present.and.returnValue(Promise.resolve());
+
+    return instance;
+  }
+}
